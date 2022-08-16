9d12f13fe1720d7eeafd8a94dc50b5e566baa6c7
Gene02 <genesisparrilez1@gmail.com>
2022-08-16 15:59:34 -0600

    escena victoria

---
+++ Assets/Genesis/ScriptsGenesis/Victory.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Victory : MonoBehaviour
+{
+    
+    void Start()
+    {
+        
+    }
+
+    
+   public void Exit()
+    {
+        Application.Quit();
+    }
+}
